ed0e4a0044d07bdfd8fdbb05d72cdbf98e3f53de
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2023-02-10 09:32:49 -0800

    Add to component list

---
+++ src/components/Edit.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import IceForm from "./IceForm";
+import PropTypes from "prop-types";
+
+function EditIceCream (props) {
+    const { iceCream } = props;
+
+function handleEditIceSubmission(event) {
+    event.preventDefault();
+    props.onEditIce({
+        flavors: event.target.flavors.value, 
+        price: event.target.price.value, 
+        quantity: event.target.quantity.value, 
+        id: iceCream.id
+})}
+
+return (
+    <React.Fragment>
+        <IceForm 
+            formSubmissionHandler =  {handleEditIceSubmission}
+            buttonText="Update IceCream" />
+        </React.Fragment>
+    );
+}
+
+EditIceCream.propTypes = {
+    iceCream: PropTypes.object,
+    onEditIce: PropTypes.func
+};
+
+export default EditIceCream;
